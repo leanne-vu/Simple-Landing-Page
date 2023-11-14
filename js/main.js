c9fe059c612beefd291f748fd7742efdb37578e3
Leanne Vu <leannevu1@yahoo.com>
2023-11-13 18:05:49 -0800

    Form resets and shows alert on submission.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ $(".inquire-button").click(function () {
   },
     'slow');
 });
+$('form').on('submit', function (event) {
+  var $form = $(this);
+  event.preventDefault();
+  alert('Success')
+  $('html,body').animate({
+    scrollTop: $(".form-section").offset().top
+  },
+    'slow');
+    $('form')[0].reset();
+});
